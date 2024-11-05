ab4e6c0528da7a51b7eb324d0d189103de760938
Marcos <112193426+Eggsbennedit@users.noreply.github.com>
2024-11-05 07:53:16 -0300

    Add files via upload

---
+++ Projeto-cards---final-main/perguntas.js
@@ -0,0 +1,60 @@
+criaCartao(
+    'Matemática',
+    'Como é calculada a área abaixo de uma função em que x tem expoente?',
+    'A área é calculada ao somar um ao expoente de x e dividi-lo pelo resultado.'
+)
+criaCartao(
+    'Programação',
+    'Qual linguagem é mais antiga: C ou Python?',
+    'C é mais antiga.'
+)
+criaCartao(
+    'Física',
+    'Como identificar a força normal em um sistema?',
+    'Normal é a força de compressão do sistema, sempre perpendicular ao apoio do bloco.'
+)
+criaCartao(
+    'Química',
+    'Como é possível identificar um carbono sp²?',
+    'É o carbono que apresenta 2 ligações sigma no subnível p².'
+)
+criaCartao(
+    'Programação',
+    'Qual a extensão de um arquivo em C#?',
+    '.cs'
+)
+criaCartao(
+    'Física',
+    'Como identificar a aceleração centrípeta?',
+    'É a componente da aceleração na direção radial da curva.'
+)
+criaCartao(
+    'Subnautica',
+    'Qual o número de leviatãs ceifadores localizados na Crash Zone?',
+    '8.'
+)
+criaCartao(
+    'Subnautica',
+    'Qual o número de naves que caíram no planeta 4546B?',
+    '3: Mercury II, Degasi e Aurora.'
+)
+criaCartao(
+    'Química',
+    'Quais são as 4 funções inorgânicas?',
+    'Ácidos, bases, sais e óxidos.'
+)
+criaCartao(
+    'Física',
+    'Qual a força da gravidade?',
+    'Gravidade não é uma força, e sim uma aceleração na direção axial de um corpo que possui massa.'
+)
+criaCartao(
+    'Física',
+    'Como é calculado o rendimento de um refrigerador?',
+    'É calculado dividindo a quantidade de calor cedido do corpo mais frio ao corpo mais quente pelo trabalho necessário para esta transferência.'
+)
+criaCartao(
+    'Programação',
+    'O que é um if statement?',
+    'Uma estrutura condicional presente em diversas linguagens de programação.'
+)
